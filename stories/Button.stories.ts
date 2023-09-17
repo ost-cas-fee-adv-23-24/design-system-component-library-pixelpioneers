2014e5bb3bf1a27410c520c28a60a4a27bd80aa4
André Ceres <andre.ceres@mgb.ch>
2023-09-17 14:09:02 +0200

    chore: test a push from IntelliJ

--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from './Button';
 
+// Button
 const meta: Meta<typeof Button> = {
   title: 'Example/Button',
   component: Button,
