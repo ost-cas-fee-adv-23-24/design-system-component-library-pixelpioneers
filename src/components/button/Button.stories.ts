70018b46edc5d170ae625281da68d1406672d9d0
André <andre.ceres@protonmail.com>
2023-09-22 15:14:03 +0200

    chore: re-add module and config it properly (#8)
    - re-add module configs
    - adjust module config base, csj and esm
    - test if module config will be bundled in the folder lib
    - clean up
    
    Co-authored-by: André Ceres <andre.ceres@mgb.ch>

--- src/components/button/Button.stories.ts
+++ src/components/button/Button.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-const meta = {
+const meta: Meta<typeof Button> = {
     title: 'Example/Button',
     component: Button,
     parameters: {
@@ -16,10 +16,10 @@ const meta = {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
-} satisfies Meta<typeof Button>;
+};
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof Button>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
