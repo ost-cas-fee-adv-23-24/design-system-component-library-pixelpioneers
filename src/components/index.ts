e40fd872e2a73e237162bcd79abefd154faa7fb6
Nico Lutz <nico.lutz@ost.ch>
2023-10-03 20:36:33 +0200

    feat: add linkbutton

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 'use client';
 
-export * from './button';
+export { Button } from './button';
+export { LinkButton } from './link-button';
---
+++ src/components/link-button/LinkButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+interface ButtonProps {
+    primary?: boolean;
+    backgroundColor?: string;
+    size?: 'small' | 'medium' | 'large';
+    label: string;
+    onClick?: () => void;
+}
+
+export const LinkButton = ({
+    primary = false,
+    size = 'medium',
+    backgroundColor,
+    label,
+    ...props
+}: ButtonProps) => {
+    const mode = primary
+        ? 'bg-red-300 hover:bg-red-500'
+        : 'bg-amber-300 hover:bg-amber-500';
+    return (
+        <a
+            type="button"
+            className={['text-white py-2 px-4 rounded', mode].join(' ')}
+            // TODO: Fix it later / size behaviour
+            //  className={[mode, `storybook-button--${size}`,  'p-10 bg-amber-800'].join(' ')}
+            style={{ backgroundColor }}
+            {...props}
+        >
+            {label}
+        </a>
+    );
+};
