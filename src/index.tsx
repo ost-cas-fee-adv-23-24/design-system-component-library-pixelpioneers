714e63e8a6f128808278b9f44d3305ecbc7b784a
André Ceres <andre.ceres@mgb.ch>
2023-10-06 20:49:02 +0200

    chore(add-eslint-package): optimize prettier - rename file .prettierrc to .prettierrc.json and format the rule - add prettier plugins: json and tailwindcss - fix size behaviour for link button - format all file with defined prettier rules

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import './index.ts';
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement,
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<React.StrictMode></React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
