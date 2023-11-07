61c2797d9b830fc1e28dd3c025d7efe59e36d72a
André Ceres <andre.ceres@mgb.ch>
2023-11-07 09:34:01 +0100

    test(setup-snapshot): config basis path

--- tests/e2e/components/icon.test.ts
+++ tests/e2e/components/icon.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('icons-element', async ({ context }, testInfo) => {
     const page = await context.newPage();
-    await page.goto('http://localhost:6006/iframe.html?id=elements-icons--single-icon-story');
+    await page.goto('/iframe.html?id=elements-icons--single-icon-story');
     await expect(page.locator('#iconMumble')).toHaveScreenshot(`${testInfo.title}.png`);
     await context.close();
 });
