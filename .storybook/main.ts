e74f2a8b659c579ed1184715ee132b860d9e31c5
André Ceres <andre.ceres@mgb.ch>
2023-11-07 08:54:22 +0100

    feat(setup-snapshot): remove no needed path config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-    stories: [
-        '../docs/*.(mdx)',
-        '../*.mdx',
-        '../src/**/*.(mdx)',
-        '../src/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)',
-    ],
+    stories: ['../docs/*.mdx', '../src/**/*.(mdx)', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)'],
     addons: [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
