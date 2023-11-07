f63d51c530149c513520ed3316c80dc2553ec8c5
Nico Lutz <nico.lutz@ost.ch>
2023-11-07 10:16:31 +0100

    feat: add typography heading and label, improve button

--- src/components/button/Button.stories.ts
+++ src/components/button/Button.stories.ts
@@ -1,40 +1,40 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Button } from './Button';
-
-const meta: Meta<typeof Button> = {
-    title: 'Components/Button',
-    component: Button,
-    parameters: {
-        layout: 'centered',
-    },
-    tags: ['autodocs'],
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
-    args: {
-        label: 'Button',
-    },
-};
-
-export default meta;
-type Story = StoryObj<typeof Button>;
-
-export const Primary: Story = {
-    args: {
-        primary: true,
-    },
-};
-
-export const Secondary: Story = {};
-
-export const Large: Story = {
-    args: {
-        size: 'large',
-    },
-};
-
-export const Small: Story = {
-    args: {
-        size: 'small',
-    },
-};
+// import type { Meta, StoryObj } from '@storybook/react';
+// import { Button } from './Button';
+//
+// const meta: Meta<typeof Button> = {
+//     title: 'Components/Button',
+//     component: Button,
+//     parameters: {
+//         layout: 'centered',
+//     },
+//     tags: ['autodocs'],
+//     argTypes: {
+//         backgroundColor: { control: 'color' },
+//     },
+//     args: {
+//         label: 'Button',
+//     },
+// };
+//
+// export default meta;
+// type Story = StoryObj<typeof Button>;
+//
+// export const Primary: Story = {
+//     args: {
+//         primary: true,
+//     },
+// };
+//
+// export const Secondary: Story = {};
+//
+// export const Large: Story = {
+//     args: {
+//         size: 'large',
+//     },
+// };
+//
+// export const Small: Story = {
+//     args: {
+//         size: 'small',
+//     },
+// };
---
+++ src/elements/typography/heading/types.ts
@@ -0,0 +1,5 @@
+export interface HeadlineProps {
+    text: string;
+    size: 'h1' | 'h2' | 'h3' | 'h4';
+    color: string;
+}
