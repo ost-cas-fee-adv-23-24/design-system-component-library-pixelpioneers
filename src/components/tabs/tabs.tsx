dc2b01211092a22fcf406742d335592c85ac6b3a
Nico Lutz <nico.lutz@ost.ch>
2023-11-19 16:34:35 +0100

    fix: make tabs design more similar to figma design

--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -1,9 +1,7 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Tab } from '@headlessui/react';
 import { TabsProps } from './types';
-import { useState } from 'react';
-import { Label } from '../typography/label/label';
-import { LabelSize } from '../typography/label';
+import { Label, LabelSize } from '../typography';
 import clsx from 'clsx';
 
 export const Tabs: FC<TabsProps> = ({ listTabs = [], isActive = 0, onTabSwitch }) => {
@@ -13,9 +11,9 @@ export const Tabs: FC<TabsProps> = ({ listTabs = [], isActive = 0, onTabSwitch }
     onTabSwitch(selectedIndex);
 
     const tabListClasses =
-        'flex flex-row justify-between items-center bg-slate-200 py-2xs px-xs rounded-s';
+        'flex flex-row justify-between items-center bg-slate-200 p-2xs rounded-s';
     const tabItemClasses =
-        'w-full cursor-pointer rounded-s whitespace-nowrap px-m py-2xs focus:outline-none border-none outline-none transition-all ease-in-out duration-400';
+        'w-full flex cursor-pointer rounded-xs whitespace-nowrap px-base py-[10px] focus:outline-none border-none outline-none transition-all ease-in-out duration-400';
 
     return (
         <div className="w-full">
