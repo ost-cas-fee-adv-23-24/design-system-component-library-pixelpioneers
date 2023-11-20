85fd2dcc4dd61e1583fe9f8e3b8544ab364b2d9f
André Ceres <andre.ceres@mgb.ch>
2023-11-20 20:29:49 +0100

    feat(smart-animation): correct types

--- src/components/navi-button/types.ts
+++ src/components/navi-button/types.ts
@@ -1,9 +1,3 @@
 import { ButtonProps } from '../button';
 
-export type NaviButtonProps = NaviButtonExtendProps &
-    Omit<ButtonProps, 'variant' | 'fill' | 'name'>;
-
-export type NaviButtonExtendProps = {
-    className?: string;
-    name?: string;
-};
+export type NaviButtonProps = Omit<ButtonProps, 'variant' | 'fill' | 'name'>;
