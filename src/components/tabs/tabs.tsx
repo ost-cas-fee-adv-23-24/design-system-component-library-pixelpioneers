de7585f965942c54ab80d11f455c6ef0cc762935
André <andre.ceres@protonmail.com>
2023-11-24 15:40:37 +0100

    feat(tabs-framer-motion): use framer motion (#67)
    - use framer motion for tabs instead (switcher)
    
    ---------
    
    Co-authored-by: André Ceres <andre.ceres@mgb.ch>
    Co-authored-by: Nico Lutz <nico.lutz@ost.ch>

--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -1,49 +1,77 @@
 import { FC, useState } from 'react';
-import { Tab } from '@headlessui/react';
 import { TabsProps } from './types';
+import { motion, useMotionValue, useSpring } from 'framer-motion';
 import { Label, LabelSize } from '../typography';
 import clsx from 'clsx';
 
-export const Tabs: FC<TabsProps> = ({ listTabs = [], isActive = 0, onTabSwitch }) => {
-    const [list] = useState(listTabs);
-    const [selectedIndex, setSelectedIndex] = useState(isActive);
-
-    onTabSwitch(selectedIndex);
+export const Tabs: FC<TabsProps> = ({ listTabs }) => {
+    const [activeItem, setActiveItem] = useState(0);
+    const [hoverItem, setHoverItem] = useState<number | undefined>(undefined);
+    const activeTabMotion = useMotionValue(0);
+    const animateSpring = useSpring(activeTabMotion, { stiffness: 400, damping: 30, mass: 1 });
 
     const tabListClasses =
-        'flex flex-row justify-between items-center bg-slate-200 p-2xs rounded-s';
-    const tabItemClasses =
-        'w-full flex cursor-pointer rounded-xs whitespace-nowrap px-base py-[10px] focus:outline-none border-none outline-none transition-all ease-in-out duration-400';
+        'flex flex-row justify-between list-none items-center bg-slate-200 p-2xs rounded-s gap-[10px]';
+    const labelClasses = (index: number) =>
+        clsx(
+            'relative z-10 whitespace-nowrap hover:cursor-pointer',
+            index === activeItem ? 'text-primary-600' : 'text-secondary-600 hover:text-slate-800',
+        );
+    const buttonClasses = clsx(
+        'bg-transparent relative m-0 border-none px-[13px] py-[10px] leading-none outline-none',
+    );
+    const activeTabClasses = (hoverRight: boolean, hoverLeft: boolean) =>
+        clsx(
+            'absolute bottom-0 left-0 right-0 top-0 rounded-xs bg-white',
+            hoverRight && 'w-[calc(100%+10px)]',
+            hoverLeft && 'ml-[-10px] w-[calc(100%+10px)]',
+        );
 
     return (
         <div className="w-full">
-            <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
-                <Tab.List className={tabListClasses}>
-                    {list.map((category, index) => (
-                        <Tab
-                            key={`${category}+${index}`}
-                            className={() =>
-                                clsx(
-                                    tabItemClasses,
-                                    selectedIndex === index
-                                        ? 'bg-white'
-                                        : 'text-secondary-600 hover:text-secondary-800',
-                                )
-                            }
+            <ol className={tabListClasses}>
+                {listTabs.map((listTab, index) => {
+                    const isActive = index === activeItem;
+                    const hoverRight = hoverItem !== undefined && hoverItem > activeItem;
+                    const hoverLeft = hoverItem !== undefined && hoverItem < activeItem;
+
+                    return (
+                        <motion.li
+                            whileTap={isActive ? { scale: 0.95 } : { opacity: 0.6 }}
+                            key={listTab.label}
                         >
-                            <Label
-                                text={category}
-                                size={LabelSize.L}
-                                className={
-                                    selectedIndex === index
-                                        ? 'text-primary-600'
-                                        : 'text-secondary-600 hover:text-slate-800'
-                                }
-                            />
-                        </Tab>
-                    ))}
-                </Tab.List>
-            </Tab.Group>
+                            <button
+                                onClick={() => {
+                                    setActiveItem(index);
+                                    listTab.onClick();
+                                }}
+                                type="button"
+                                className={buttonClasses}
+                            >
+                                {isActive && (
+                                    <motion.div
+                                        layoutId="segmentedControlActive"
+                                        className={activeTabClasses(hoverRight, hoverLeft)}
+                                        style={{
+                                            transform: `translateX(${animateSpring.get()}px)`,
+                                        }}
+                                    />
+                                )}
+                                <motion.div
+                                    onHoverStart={() => setHoverItem(index)}
+                                    onHoverEnd={() => setHoverItem(undefined)}
+                                >
+                                    <Label
+                                        text={listTab.label}
+                                        size={LabelSize.L}
+                                        className={labelClasses(index)}
+                                    />
+                                </motion.div>
+                            </button>
+                        </motion.li>
+                    );
+                })}
+            </ol>
         </div>
     );
 };
--- src/components/tabs/types.ts
+++ src/components/tabs/types.ts
@@ -1,5 +1,9 @@
 export interface TabsProps {
-    listTabs: Array<string>;
+    listTabs: ListTab[];
     isActive: number;
-    onTabSwitch: (index: number) => void;
 }
+
+export type ListTab = {
+    label: string;
+    onClick: () => void;
+};
