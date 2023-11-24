bb602eadc180d7f5383be97ffd8a7cdd6e2ccd8f
André Ceres <andre.ceres@mgb.ch>
2023-11-24 09:37:39 +0100

    feat(tabs-framer-motion): check and improve style

--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -10,13 +10,12 @@ export const Tabs: FC<TabsProps> = ({ listTabs }) => {
 
     const tabListClasses =
         'flex flex-row justify-between list-none items-center bg-slate-200 p-2xs rounded-s';
-    // const tabItemClasses =
-    ('w-full flex cursor-pointer rounded-xs whitespace-nowrap px-base py-[10px] focus:outline-none border-none outline-none transition-all ease-in-out duration-400');
     const labelClasses = (index: number) =>
         clsx(
-            'relative whitespace-nowrap',
+            'relative whitespace-nowrap hover:cursor-pointer focus:cursor-pointer',
             index === activeItem ? 'text-primary-600' : 'text-secondary-600 hover:text-slate-800',
         );
+
     return (
         <div className="w-full">
             <ol className={tabListClasses}>
@@ -24,6 +23,7 @@ export const Tabs: FC<TabsProps> = ({ listTabs }) => {
                     const isActive = index === activeItem;
                     const hoverRight = hoverItem !== undefined && hoverItem > activeItem;
                     const hoverLeft = hoverItem !== undefined && hoverItem < activeItem;
+
                     return (
                         <motion.li
                             className={isActive || index === activeItem - 1 ? '' : ''}
@@ -37,15 +37,15 @@ export const Tabs: FC<TabsProps> = ({ listTabs }) => {
                                 }}
                                 type="button"
                                 className={clsx(
-                                    'bg-transparent relative m-0 border-none px-[30px] py-[7px] leading-none outline-none hover:cursor-pointer focus:cursor-pointer',
+                                    'bg-transparent relative m-0 border-none px-[13px] py-[10px] leading-none outline-none',
                                     isActive && 'group',
                                 )}
                             >
                                 {isActive && (
                                     <motion.div
-                                        layoutId="SegmentedControlActive"
+                                        layoutId="segmentedControlActive"
                                         className={clsx(
-                                            ' absolute bottom-0 left-0 right-0 top-0 rounded-xs bg-white',
+                                            'absolute bottom-0 left-0 right-0 top-0 rounded-xs bg-white',
                                             hoverRight && 'w-[calc(100%+10px)]',
                                             hoverLeft && 'ml-[-10px] w-[calc(100%+10px)]',
                                         )}
@@ -74,33 +74,6 @@ export const Tabs: FC<TabsProps> = ({ listTabs }) => {
                     );
                 })}
             </ol>
-            {/* <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
-                <Tab.List className={tabListClasses}>
-                    {list.map((category, index) => (
-                        <Tab
-                            key={`${category}+${index}`}
-                            className={() =>
-                                clsx(
-                                    tabItemClasses,
-                                    selectedIndex === index
-                                        ? 'bg-white'
-                                        : 'text-secondary-600 hover:text-secondary-800',
-                                )
-                            }
-                        >
-                            <Label
-                                text={category}
-                                size={LabelSize.L}
-                                className={
-                                    selectedIndex === index
-                                        ? 'text-primary-600'
-                                        : 'text-secondary-600 hover:text-slate-800'
-                                }
-                            />
-                        </Tab>
-                    ))}
-                </Tab.List>
-            </Tab.Group> */}
         </div>
     );
 };
