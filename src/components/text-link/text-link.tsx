9d5e63216661ec09d256f391bf08dd700e8915c7
Nico Lutz <nico.lutz@ost.ch>
2023-12-06 16:44:38 +0100

    fix: textlink animation issues

--- src/components/text-link/text-link.tsx
+++ src/components/text-link/text-link.tsx
@@ -4,18 +4,14 @@ import clsx from 'clsx';
 import { Label, LabelSize, LabelType } from '../typography';
 
 export const TextLink: FC<TextLinkProps> = ({ label, link, className, ...props }) => {
-    // TODO: IMPROVE ANIMATION
     const linkClasses = clsx('flex justify-center text-primary-600', className);
-    const labelClasses =
-        'underline underline-offset-2 hover:underline-offset-4 hover:decoration-primary-200';
-    const transitionClasses = 'transition-all duration-350';
+    const labelClasses = clsx(
+        'underline underline-offset-2 hover:decoration-primary-200 hover:underline-offset-4',
+        'transition-all duration-300',
+    );
     return (
         <a className={linkClasses} href={link} {...props}>
-            <Label
-                type={LabelType.SPAN}
-                size={LabelSize.S}
-                className={clsx(labelClasses, transitionClasses)}
-            >
+            <Label type={LabelType.SPAN} size={LabelSize.S} className={labelClasses}>
                 {label}
             </Label>
         </a>
--- src/components/typography/label/label.tsx
+++ src/components/typography/label/label.tsx
@@ -10,7 +10,7 @@ export const Label: FC<LabelProps> = ({
     ...props
 }) => {
     const labelClasses = clsx(
-        'wrap-label font-default font-semibold leading-none transition-opacity',
+        'wrap-label font-default font-semibold leading-none',
         {
             s: 'text-xs',
             m: 'text-sm',
