2877bb4eac127075885ab754be3cefa1a7f1b5f5
André Ceres <andre.ceres@mgb.ch>
2024-04-14 09:41:50 +0200

    feat(add-image-preview): add small avatar edit size for mobile view

--- src/components/avatar/edit-avatar.stories.ts
+++ src/components/avatar/edit-avatar.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { EditAvatar } from './edit-avatar';
+import { AvatarEditSize } from './types';
 
 const meta: Meta<typeof EditAvatar> = {
     title: 'Components/Edit Avatar',
@@ -11,17 +12,28 @@ const meta: Meta<typeof EditAvatar> = {
     argTypes: {
         onEdit: { action: 'clicked' },
     },
-    args: {},
+    args: {
+        size: AvatarEditSize.XL,
+    },
 };
 
 export default meta;
 type EditAvatarStory = StoryObj<typeof EditAvatar>;
 
 export const Default: EditAvatarStory = {
-    name: 'Edit Mode',
+    name: 'Edit Mode XL',
     args: {
         alt: 'donald-duck-avatar',
         src: 'https://easydrawingguides.com/wp-content/uploads/2019/09/Donald-Duck-10.png',
+    },
+};
+
+export const EditModeSizeL: EditAvatarStory = {
+    name: 'Edit Mode L',
+    args: {
+        alt: 'donald-duck-avatar',
+        src: 'https://easydrawingguides.com/wp-content/uploads/2019/09/Donald-Duck-10.png',
+        size: AvatarEditSize.L,
     },
 };
 
