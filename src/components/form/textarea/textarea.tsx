21811d8c1bd55484f84ef4e6781008d2e98da0c5
Nico Lutz <nico.lutz@ost.ch>
2024-05-07 18:21:08 +0200

    fix: textarea ref props

--- src/components/form/textarea/textarea.tsx
+++ src/components/form/textarea/textarea.tsx
@@ -1,28 +1,26 @@
 'use client';
-import { FC, ForwardedRef, forwardRef } from 'react';
+import { FC, forwardRef } from 'react';
 import { TextareaProps } from './types';
 import clsx from 'clsx';
 
-export const Textarea: FC<TextareaProps> = forwardRef(
-    ({ className, ...props }, ref: ForwardedRef<HTMLTextAreaElement>) => (
-        <div className={'relative'}>
-            <textarea
-                ref={ref}
-                className={clsx(
-                    'placeholder:text-secondary-500',
-                    'h-[160px] w-full',
-                    'bg-secondary-100 text-lg',
-                    'transition-all duration-300 ease-in-out',
-                    'rounded-s p-s',
-                    'resize-none',
-                    'border border-secondary-200',
-                    'ring-1 outline-none ring-secondary-200',
-                    'hover:ring-2 hover:ring-secondary-300',
-                    'focus:ring-primary-600',
-                    className,
-                )}
-                {...props}
-            />
-        </div>
-    ),
-);
+export const Textarea: FC<TextareaProps> = forwardRef(({ className, ...props }, ref) => (
+    <div className={'relative'}>
+        <textarea
+            ref={ref}
+            className={clsx(
+                'placeholder:text-secondary-500',
+                'h-[160px] w-full',
+                'bg-secondary-100 text-lg',
+                'transition-all duration-300 ease-in-out',
+                'rounded-s p-s',
+                'resize-none',
+                'border border-secondary-200',
+                'ring-1 outline-none ring-secondary-200',
+                'hover:ring-2 hover:ring-secondary-300',
+                'focus:ring-primary-600',
+                className,
+            )}
+            {...props}
+        />
+    </div>
+));
